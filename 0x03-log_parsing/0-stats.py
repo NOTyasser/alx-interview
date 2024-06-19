0d9629315c4ea0ea55afc3af79305edb0f6f7ddb
NOTyasser <125504642+NOTyasser@users.noreply.github.com>
2024-06-20 01:31:30 +0200

    all

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,68 @@
 #!/usr/bin/python3
+"""0-state.py"""
 
 import sys
+from typing import List
+import re
+
+REGEX_PATTERN = re.compile(r"(^(?:[0-9]{1,3}\.){3}[0-9]{1,3}) - \[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+\]\s\"GET \/projects\/260 HTTP\/1.1\"\s(\d{3})\s(\d+)$")  # noqa
+
+total_file_size: int = 0
+counter: int = 0
+possible_states: List[List[int]] = [
+    [200, 0], [301, 0],
+    [400, 0], [401, 0],
+    [403, 0], [404, 0],
+    [405, 0], [500, 0]
+]
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
+def print_status(status_arr: List[List[int]]) -> None:
+    """print_status
     Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
+        status_arr (List[List[int]]): status array
     """
+    for status in status_arr:
+        if status[1] > 0:
+            print(f"{status[0]}: {status[1]}")
 
+
+def print_infos(total_file_size: int, possible_states: List[List[int]]) -> None:  # noqa
+    """print_infos
+    Args:
+        total_file_size (int): total file size
+        possible_states (List[List[int]]): possible states
+    """
     print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+    print_status(possible_states)
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+def update_possible_states(poss_stats: List[List[int]], matched_stat: int) -> None:  # noqa
+    """update_possible_states
+    Args:
+        poss_stats (List[List[int]]): possible states
+        matched_stat (int): matched status
+    """
+    for stat in poss_stats:
+        if stat[0] == matched_stat:
+            stat[1] += 1
+            break
+
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+        matched = re.search(REGEX_PATTERN, line)
+        if not matched:
+            continue
+        total_file_size += int(matched.group(3))
+        counter += 1
+        update_possible_states(possible_states, int(matched.group(2)))
+        if counter == 10:
+            counter = 0
+            print_infos(total_file_size, possible_states)
 
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+except Exception as e:
+    pass
 
 finally:
-    print_msg(dict_sc, total_file_size)
+    print_infos(total_file_size, possible_states)
