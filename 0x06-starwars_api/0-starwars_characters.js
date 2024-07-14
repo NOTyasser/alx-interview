725ad81a0689cf058470f0b83cdaa4daa231b9f7
NOTyasser <125504642+NOTyasser@users.noreply.github.com>
2024-07-14 14:49:15 +0200

    all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,17 @@
 #!/usr/bin/node
-/**
- * A script that prints all the characters of a Star Wars Movie.
- */
+
 const request = require('request');
-const movieId = process.argv[2];
 
-if (!movieId) {
-  console.log('Please provide a movie ID');
-  process.exit(1);
-}
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request.get(url, async (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    try {
-      const fetchCharacterName = (characterUrl) => {
-        return new Promise((resolve, reject) => {
-          request.get(characterUrl, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-              resolve(JSON.parse(body).name);
-            } else {
-              reject(new Error('Character information request failed!'));
-            }
-          });
-        });
-      };
-      for (const characterUrl of JSON.parse(body).characters) {
-        console.log(await fetchCharacterName(characterUrl));
-      }
-    } catch (err) {
-      console.log('Exception:', err);
-    }
-  } else {
-    console.log('Movie information request failed!');
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
